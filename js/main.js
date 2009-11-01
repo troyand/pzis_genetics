daf15c38ec59b86252888e09b7aa61c60677af74
troyand <troyanovsky@gmail.com>
2009-11-01 17:42:35 +0200

    canvas plotter added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ var selectionType;
 var crossingoverProbability;
 var mutationProbability;
 var stopCriterion;
+var canvasContext;
+var time = 0;
 
 $(document).ready(function(){
         $("#tabs").tabs();
@@ -18,6 +20,7 @@ $(document).ready(function(){
             });
         updateOptions();
         renderOptions();
+        initCanvas();
         });
 
 function updateOptions(){
@@ -47,4 +50,41 @@ function renderOptions(){
     optionsRenderHelper("Crossing over", crossingoverProbability);
     optionsRenderHelper("Mutation", mutationProbability);
     optionsRenderHelper("Stop criterion", stopCriterion);
+}
+
+function initCanvas(){
+    var canvas = document.getElementById("graph-canvas");
+    if (canvas.getContext) {
+        canvasContext = canvas.getContext("2d");
+
+        canvasContext.fillStyle = "rgb(200,0,0)";
+        canvasContext.fillRect (10, 10, 55, 50);
+
+        canvasContext.fillStyle = "rgba(0, 0, 200, 0.5)";
+        canvasContext.fillRect (30, 30, 55, 50);
+
+        canvasContext.fillStyle = "rgb(200,200,200)";
+        canvasContext.fillRect(0, 0, 640, 480);
+
+        //canvasContext.beginPath();
+        //canvasContext.arc(320, 240, 10, 0, 2*Math.PI, 0);
+        //canvasContext.fill();
+        setInterval("canvasRenderLoop()", 40);
+    }
+}
+
+function canvasRenderLoop(){
+    canvasContext.fillStyle = "rgb(255,255,255)";
+    //canvasContext.fillRect(0, 0, 640, 480);
+    //canvasContext.translate(time, 0);
+    var width = 640;
+    var height = 480;
+    canvasContext.fillRect(0+(time % width), 0, 1, height);
+
+    canvasContext.fillStyle = "rgba(255,0,0,0.5)";
+    canvasContext.fillRect(0+((time + 1) % width), 0, 1, height);
+
+    canvasContext.fillStyle = "rgba(0, 0, 200, 0.5)";
+    canvasContext.fillRect(0+(time % width), 0.5*height*(1-Math.sin(0.02*time*Math.exp(0.0003*time))*Math.exp(-0.001*time)), 2, 2);
+    time += 1;
 }
