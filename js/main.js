31607d5e5d97abb7a3c4711993266938f2a2415a
troyand <troyanovsky@gmail.com>
2009-11-01 21:17:17 +0200

    canvas plotter rendering enchancements: start/pause/stop buttons

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ var mutationProbability;
 var stopCriterion;
 var canvasContext;
 var time = 0;
+var updateInterval;
+var renderIntervalID = null;
 
 $(document).ready(function(){
         $("#tabs").tabs();
@@ -21,6 +23,28 @@ $(document).ready(function(){
         updateOptions();
         renderOptions();
         initCanvas();
+        
+        $(".ui-icon-stop").click(function() {
+            time = 0;
+            clearInterval(renderIntervalID);
+            renderIntervalID = null;
+            });
+
+        $(".ui-icon-pause").click(function() {
+            clearInterval(renderIntervalID);
+            renderIntervalID = null;
+            });
+
+        $(".ui-icon-play").click(function() {
+                if(time == 0){
+                    initCanvas();
+                }
+                else{
+                    if(renderIntervalID == null){
+                        renderIntervalID = setInterval("canvasRenderLoop()", updateInterval);
+                    }
+                }
+            });
         });
 
 function updateOptions(){
@@ -33,6 +57,7 @@ function updateOptions(){
     crossingoverProbability = $("#crossing-over-percent").val();
     mutationProbability = $("#mutation-percent").val();
     stopCriterion = $("#stop-criterion-chooser-selector").val();
+    updateInterval = $("#update-interval").val();
 }
 
 function optionsRenderHelper(text, value){
@@ -50,6 +75,7 @@ function renderOptions(){
     optionsRenderHelper("Crossing over", crossingoverProbability);
     optionsRenderHelper("Mutation", mutationProbability);
     optionsRenderHelper("Stop criterion", stopCriterion);
+    optionsRenderHelper("Update interval", updateInterval);
 }
 
 function initCanvas(){
@@ -69,11 +95,22 @@ function initCanvas(){
         //canvasContext.beginPath();
         //canvasContext.arc(320, 240, 10, 0, 2*Math.PI, 0);
         //canvasContext.fill();
-        setInterval("canvasRenderLoop()", 40);
+        renderIntervalID = setInterval("canvasRenderLoop()", updateInterval);
     }
 }
 
 function canvasRenderLoop(){
+    function sinExp(t){
+        return Math.sin(0.02*time*Math.exp(0.0003*t))*Math.exp(-0.001*t);
+        //return Math.sin(0.01*t);
+    }
+    function myCos(t){
+        return Math.cos(0.01*t);
+    }
+    function renderPlot(f,t,color){
+        canvasContext.fillStyle = color;
+        canvasContext.fillRect(0 + (t % width), 0.5*height*(1 - f(t)) - 1, 1, 2);
+    }
     canvasContext.fillStyle = "rgb(255,255,255)";
     //canvasContext.fillRect(0, 0, 640, 480);
     //canvasContext.translate(time, 0);
@@ -84,7 +121,11 @@ function canvasRenderLoop(){
     canvasContext.fillStyle = "rgba(255,0,0,0.5)";
     canvasContext.fillRect(0+((time + 1) % width), 0, 1, height);
 
-    canvasContext.fillStyle = "rgba(0, 0, 200, 0.5)";
-    canvasContext.fillRect(0+(time % width), 0.5*height*(1-Math.sin(0.02*time*Math.exp(0.0003*time))*Math.exp(-0.001*time)), 2, 2);
+    //canvasContext.fillStyle = "rgba(0, 0, 200, 0.5)";
+    //canvasContext.fillRect(0+(time % width), 0.5*height*(1-fRender(time))-1, 1, 2 );
+    renderPlot(sinExp, time, "rgba(0, 0, 200, 0.5)");
+
+    renderPlot(myCos, time, "rgba(0, 200, 0, 0.5)");
+    //alert(Math.abs(fRender(time)-fRender(time+2)));
     time += 1;
 }
